f54942ff8b4f2dfd0635a324c5305919161f8c88
Hamza-1997 <hamzaakif97@gmail.com>
2021-12-28 17:48:27 +0500

    refactored file structure

--- app.js
+++ app.js
@@ -1,39 +1,16 @@
-// const http = require("http");
-// const express = require("express");
-// const app = express();
-// const cors = require("cors");
-// const mongoose = require("mongoose");
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const config = require("./utils/config");
+const mongoose = require("mongoose");
+const blogRouter = require("./controllers/blog");
 
-// const blogSchema = new mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number,
-// });
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl);
 
-// const Blog = mongoose.model("Blog", blogSchema);
+app.use(cors());
+app.use(express.json());
+app.use("/api/blogs", blogRouter);
 
-// const mongoUrl = "mongodb://localhost/bloglist";
-// mongoose.connect(mongoUrl);
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.get("/api/blogs", (request, response) => {
-//   Blog.find({}).then((blogs) => {
-//     response.json(blogs);
-//   });
-// });
-
-// app.post("/api/blogs", (request, response) => {
-//   const blog = new Blog(request.body);
-
-//   blog.save().then((result) => {
-//     response.status(201).json(result);
-//   });
-// });
-
-// const PORT = 3003;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+module.exports = app;
